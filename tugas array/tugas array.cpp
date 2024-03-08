27ec2af3e438705a679791aaa4f0ded4a089b58a
halimbagusalhakim <halsat132@gmail.com>
2024-03-08 16:35:37 +0700

    membuat fungsi berisi looping perintah untuk menginput banyak elemen dan looping menampilkan data ke-

--- tugas array/tugas array.cpp
+++ tugas array/tugas array.cpp
@@ -9,13 +9,29 @@ int arr[20];
 int n;
 
 void input() {
+    while (true) {
+        cout << "masukan banyaknya elemen pada array :";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "\nArray hanya dapat mempunyai maksimal 20 elemen\n";
+        }
+    }
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << " Masukan elemen Array" << endl;
+    cout << "=====================" << endl;
 
+    for (int i = 0; i < n; i++) {
+        cout << "Data ke-" << (i + 1) << ":";
+        cin >> arr[i];
+    }
 }
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+
+
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
